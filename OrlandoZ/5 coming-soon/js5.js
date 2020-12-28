836f813780f8edcaeb028cf8680a8270284cc551
OrlandoZuñiga <orlando.zuniga@sinco.com.co>
2020-12-28 18:18:51 -0500

    Ejercicio V1

---
+++ OrlandoZ/5 coming-soon/js5.js
@@ -0,0 +1,60 @@
+
+const left = document.querySelector(".left");
+const titulo1 = document.querySelector(".titulo1");
+const titulo2 = document.querySelector(".titulo2");
+const titulo3 = document.querySelector(".titulo3");
+const texto = document.querySelector(".texto");
+const right = document.querySelector(".right");
+const email = document.querySelector(".email");
+var mail = document.forms[correo]
+var ok = true;
+
+
+
+texto.innerHTML = "Hello fellow shopper! We´re currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.";
+titulo1.innerHTML = "W E ´ R E";
+titulo2.innerHTML = "C O M I N G";
+titulo3.innerHTML = "S O O N";
+
+flecha.addEventListener('click', validar(mail));
+
+function validar(mail){
+   
+
+}
+
+
+
+
+
+
+
+/*
+
+next.addEventListener('click', cambios())
+*/
+
+
+
+/*
+nombres.innerHTML = "Tanya Sinclair";
+profesion.innerHTML = "UX Engineer"
+foto.src = "./img/image-tanya.jpg";
+
+function cambios(){
+    if (tanya){
+        texto.innerHTML = " “If you want to lay the best foundation possible i´d recommend taking this course. The depth  the instructors go into is incredible. i nom feel so confident about starting up as a profesional developer.”"
+        nombres.innerHTML = "John Takpor ";
+        profesion.innerHTML = "Junior Front-end Developer";
+        foto.src = "./img/image-john.jpg";
+        tanya=false;
+    }else{
+        texto.innerHTML= " I´ve been interested in coding for a while but never taken the jump, until now. I couldn´t recommend this course enough. I´m now in the job of my dreams and so excited about the future.”"
+        nombres.innerHTML = "Tanya Sinclair";
+        profesion.innerHTML = "UX Engineer";
+        foto.src = "./img/image-tanya.jpg";
+        tanya = true;
+    }
+}*/
+
+

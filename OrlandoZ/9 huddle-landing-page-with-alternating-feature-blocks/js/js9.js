3ce079555c7505c5a4825ed36589941b4fb59cc6
OrlandoZuñiga <orlando.zuniga@sinco.com.co>
2021-01-18 16:37:46 -0500

    Ejecicio9 con js

---
+++ OrlandoZ/9 huddle-landing-page-with-alternating-feature-blocks/js/js9.js
@@ -0,0 +1,65 @@
+ const tituloCabecera =  document.querySelector("#tituloCabecera");
+ const parrafoCabecera =  document.querySelector("#parrafoCabecera");
+ const subtitulo1 =  document.querySelector(".subtitulo1");
+ const parrafoMarco1 =  document.querySelector(".parrafoMarco1");
+ const subtitulo2 =  document.querySelector(".subtitulo2");
+ const parrafoMarco2 =  document.querySelector(".parrafoMarco2");
+ const subtitulo3 =  document.querySelector(".subtitulo3");
+ const parrafoMarco3 =  document.querySelector(".parrafoMarco3");
+
+ tituloCabecera.innerHTML = " Build The Comunity Your Fans Will Love"
+ parrafoCabecera.innerHTML = " Huddle re-images the way we build communities. you have a voice, but so does your audience. Create connectrion with your users as you engage in genuine discussion.";
+ 
+ subtitulo1.innerHTML = "Grow Together";
+ parrafoMarco1.innerHTML = "Generate meaningful discussion with your audience  and build a strong, loyal comunity. Think of the insightful  conversations you miss out on with a feedback form.";
+
+
+ subtitulo2.innerHTML = "Flowing Conversations";
+ parrafoMarco2.innerHTML = "You wouldn´t paginate conversation in real life,  so why do it online? Our threads have just-in-time loaading for  more natural flow.";
+ 
+ subtitulo3.innerHTML = "Your Uses";
+ parrafoMarco3.innerHTML = "It takes no time at all to integrate Huddle with your  app´s authentication solution. This means, once signed in to your app,  your users can start chatting inmediatly.";
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+function mensaje(){
+    alert("boton try it")
+}
+
+document.getElementById("tryit").onclick=function(){
+    mensaje();
+}
+
+function mensaje2(){
+    alert("boton GET STARTER FOR FREE")
+}
+
+document.getElementById("prom").onclick=function(){
+    mensaje2();
+}
+
+document.getElementById("prom2").onclick=function(){
+    mensaje2();
+}
+
+document.getElementById("face").onclick=function(){
+    window.open("http://facebook.com", "Página Facebook");
+}
+
+document.getElementById("instagram").onclick=function(){
+    window.open("http://instagram.com", "Página Facebook");
+}
+
+document.getElementById("twitter").onclick=function(){
+    window.open("http://twitter.com", "Página Facebook");
+}
---
+++ OrlandoZ/9 huddle-landing-page-with-alternating-feature-blocks/readme.md
@@ -0,0 +1,3 @@
+casi completo 85% aprox
+pendiente por corregir logo del footer
+centrar boton encima de footer

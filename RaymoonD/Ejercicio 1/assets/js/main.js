3cac9907233b6e20619622fb5fde7cdf73e8b176
Raymoon Samir <Raymoon Samir Durango>
2021-01-18 15:01:19 -0500

    Commit inicial

---
+++ RaymoonD/Ejercicio 1/assets/js/main.js
@@ -0,0 +1,8 @@
+function mostrarRedes(){
+    var social = document.getElementById("social-networks");
+    if (social.style.display === "none") {
+        social.style.display = "flex";        
+    }else{
+        social.style.display ="none";
+    }
+}

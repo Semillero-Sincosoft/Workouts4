105ca69a22027ab2547c44f4e60980299725cf57
Raymoon Samir <Raymoon Samir Durango>
2021-01-19 16:59:31 -0500

    Ajustes en social,posiciones e imagenes

--- RaymoonD/1-article-preview-component/assets/js/main.js
+++ RaymoonD/1-article-preview-component/assets/js/main.js
@@ -1,9 +1,16 @@
 //Función para mostrar las redes sociales
+
 function mostrarRedes(){
+    console.time("mostrarR")
     var social = document.getElementById("social-networks");
     if (social.style.display === "none") {
-        social.style.display = "flex";        
+        
+        social.style.display = "flex";
+                
     }else{
+        
         social.style.display ="none";
+        
     }
+    console.timeEnd('mostrarR')
 }

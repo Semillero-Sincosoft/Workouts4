7cc0b823dd966f4856f4fe0fdec6e78625f448b1
[AlanMontoya2021] <alan.montoya@sinco.com.co>
2021-01-28 17:27:31 -0500

    28/01/2021

--- Alan/3. Coding-Bootcamp-Testimonials-Slider/main.js
+++ Alan/3. Coding-Bootcamp-Testimonials-Slider/main.js
@@ -1,6 +1,6 @@
 let contenido=[{foto:'img/image-tanya.jpg',aporte:"“ I’ve been interested in coding for a while but never taken the jump,until now. I couldn’t recommend this course enough. I’m now in the jobof my dreams and so excited about the future.”",nombre:'Tanya Sinclair ',dedicacion:'UX Engineer'},
 {foto:'img/image-john.jpg',aporte:"“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",nombre:'John Tarkpor',dedicacion:'Junior Front-end Developer'},
-{foto:'img/image-riqui.jpeg',aporte:"“Riqui puig is a young footballer who with a smile and effort wants to get ahead in the club of his dreams.“",nombre:'Riqui Piug',dedicacion:'Master in soccer '}];
+{foto:'img/image-riqui.jpeg',aporte:"“Riqui Puig is a young footballer who with a smile and effort wants to get ahead in the club of his dreams.“",nombre:'Riqui Puig',dedicacion:'Master in soccer '}];
 let contador=0;
 
 let foto = document.querySelector('.image');

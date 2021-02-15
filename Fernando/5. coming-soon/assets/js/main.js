2be47fa11676228045c3eb2ddf2b5dedd6317e3c
Luis Fernando <Luis Fernando Amado>
2021-02-15 15:53:06 -0500

    ejercicio6 final

--- Fernando/5. coming-soon/assets/js/main.js
+++ Fernando/5. coming-soon/assets/js/main.js
@@ -1,42 +1,42 @@
+//VALIDACION INPUT
 const form = document.getElementById('form') 
 const input = document.querySelector('.email')
 const expressions = {
-	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+	email: /^[-\w.%+]{​​​​1,64}​​​​@(?:[A-Z0-9-]{​​​​1,63}​​​​\.){​​​​1,125}​​​​[A-Z]{​​​​2,63}​​​​$/i
+}
+const campos={
+    email:false
 }
 const alerta = document.querySelector(".alert")
-let email =true ;
 const button = document.querySelector(".button")
+
 function validateForm(e){
 
    expressions.email.test(e.target.value)?
    (alerta.style.display="none",
     input.classList.remove('false'),
-    email=false,
-    button.classList.remove('errorButton')
+    button.classList.remove('errorButton'),
+    campos['email']=true
     ):(
-      input.classList.add('false'),
-      alerta.style.display="block",
-      email=false),
-      button.classList.add('errorButton');
-   
+        input.classList.add('false'),
+        alerta.style.display="block",
+        campos['email']=false,
+        button.classList.add('errorButton')); 
 }
 input.addEventListener('keyup', validateForm);
 form.addEventListener('submit',(e)=>{
-    let success= document.querySelector(".success")
+let success= document.querySelector(".success")
 e.preventDefault();
-if( email=true && input.value!="" ){
-    form.reset();
-    success.style.display="block";
-    
+ campos.email ?
+    (form.reset(),
+    success.style.display="block",
     setTimeout(() =>{
-        success.style.display="none";
-    }, 2000)
-    
-}
-else{
-    input.classList.add('false');
-    success.style.display="none";
-    alerta.style.display="block";
-    email=false;
-}
+        success.style.display="none"
+    }, 2000))
+    :
+    (input.classList.add('false'),
+    success.style.display="none",
+    alerta.style.display="block");
+
 })
+//TRANSICION

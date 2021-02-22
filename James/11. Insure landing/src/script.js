231e6bc92abbabf21e1f9c58218d8c2a2e520a35
JamesChavess <alfonsoforero1022@gmail.com>
2021-02-21 23:10:54 -0500

    Up to day, ready

---
+++ James/11. Insure landing/src/script.js
@@ -0,0 +1,55 @@
+const body = document.getElementById('body');
+
+const container = document.createElement('main');
+
+const navBar = document.createElement('header');
+const logo = document.createElement('img');
+const options = document.createElement('ul');
+const option1 = document.createElement('li');
+const option2 = document.createElement('li');
+const option3 = document.createElement('li');
+const planBtn1 = document.createElement('button');
+const menu = document.createElement('img')
+
+//place following code inside of creator function when it works
+const popUpMenu = document.createElement('nav');
+const h = document.createElement('span');
+const b = document.createElement('span');
+const a = document.createElement('span');
+const v = document.createElement('button');
+//
+const intro = document.createElement('section');
+const famM = document.createElement('img');
+const patLeftM = document.createElement('img');
+const patRightM = document.createElement('img');
+const patLeft = document.createElement('img');
+const patRight = document.createElement('img');
+const fam = document.createElement('img');
+const grayBar1 = document.createElement('hr');
+const introH1 = document.createElement('h1');
+const introP= document.createElement('p');
+const planBtn1 = document.createElement('button');
+
+const pitch = document.createElement('section');
+const grayBar2 = document.createElement('hr');
+const pitchH2 = document.createElement('h2');
+const pitchP = document.createElement('p');
+
+const snappy = document.createElement('article');
+const snappyImg = document.createElement('img');
+const snappyH3 = document.createElement('h1');
+const snappyP = document.createElement('P');
+const affordable = document.createElement('article');
+const affordableImg = document.createElement('img');
+const affordableH3 = document.createElement('h1');
+const affordableP = document.createElement('P');
+const pplFirst = document.createElement('article');
+const pplFirstImg = document.createElement('img');
+const pplFirstH3 = document.createElement('h1');
+const pplFirstP = document.createElement('P');
+const howWeWork = document.createElement('article');
+const howWeWorkImg = document.createElement('img');
+const howWeWorkH3 = document.createElement('h1');
+const howWeWorkP = document.createElement('P');
+
+
